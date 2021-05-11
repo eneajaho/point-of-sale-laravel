9ae75dd17c8a8db96d692044a411e51de1520fb6
Etjen Disha <etjendisha@gmail.com>
2021-05-11 23:01:27 +0200

    Added Model and Migration for products

---
+++ database/migrations/2021_05_11_205549_create_products_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->text('description');
+            $table->string('name');
+            $table->integer('price');
+            $table->integer('low_stock');
+            $table->integer('optimal_stock');
+            $table->string('barcode');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
