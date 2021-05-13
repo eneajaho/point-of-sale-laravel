39bd42b4de1361c524d519650c1cad91cd0394f1
Etjen Disha <etjendisha@gmail.com>
2021-05-13 15:13:30 +0200

    Add Controller for Products

--- database/migrations/2021_05_11_205549_create_products_table.php
+++ database/migrations/2021_05_11_205549_create_products_table.php
@@ -15,8 +15,8 @@ class CreateProductsTable extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
-            $table->text('description');
-            $table->string('name');
+            $table->text('description')->default('');
+            $table->string('name')->default('');
             $table->integer('price');
             $table->integer('low_stock');
             $table->integer('optimal_stock');
